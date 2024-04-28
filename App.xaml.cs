cc553c00b115510c331690e6711dd2723d2c0f94
Neonalig <codychook@gmail.com>
2024-04-28 11:41:39 +0800

    Created new WPF project

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MorcuTool;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+}
